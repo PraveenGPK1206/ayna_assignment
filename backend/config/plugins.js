4fdaf8c305a3302331f9fe341f578b31f92f13c1
Praveen Kumar <kumargundrala@gmail.com>
2025-01-23 15:38:57 +0530

    commit3

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = {
+    'users-permissions': {
+      config: {
+        jwtSecret: process.env.JWT_SECRET || '2d4f3154a41765bb7e9a8136d9f1834b',
+      },
+    },
+  };
+  
