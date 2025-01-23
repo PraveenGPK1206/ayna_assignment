bf56148d170732379ffb3012866e7e8c8e9dc6bf
Praveen Kumar <kumargundrala@gmail.com>
2025-01-23 15:29:17 +0530

    commit 2

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['defaultKeyA', 'defaultKeyB']), // Optional default keys if environment variable is not set
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
