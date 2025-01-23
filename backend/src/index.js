8223f8c0b931a6a5ca20f7d12dd14fc3cf86758a
PraveenGPK1206 <138776415+PraveenGPK1206@users.noreply.github.com>
2025-01-23 16:12:25 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ module.exports = {
   bootstrap({ strapi }) {
     const io = new Server(strapi.server.httpServer, {
       cors: {
-        origin: "http://localhost:3000", // React frontend URL
+        origin: "https://clever-parfait-43be20.netlify.app/", // React frontend URL
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true,
